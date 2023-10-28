2036937600980f147969bd9dad3ad35149a63a4a
aquib143 <aquibrahmani143@gmail.com>
2023-10-28 15:10:18 +0530

    Uploaded Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "../src/img/login.png"
+import email from "../src/img/email.png"
+import pass from "../src/img/password.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <div className='sub-main'>
+        <div>
+          <div className='imgs'>
+            <div className='container-image'>
+              <img src={logo} alt='' className='profile' />
+            </div>
+          </div>
+
+          <div>
+            <h1>LOGIN</h1>
+            <div>
+              <img src={email} alt='' className='email' />
+              <input type="text" placeholder='Enter Your Emial' className='name' />
+            </div>
+
+            <br />
+            <div>
+              <img src={pass} alt='' className='email' />
+              <input type="password" placeholder='Enter Your Password' className='name' />
+            </div>
+
+            <div className='login-button'>
+              <button style={{ boxShadow: "5px 5px 8px 5px rgb(207, 200, 200)" }}>Login</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
